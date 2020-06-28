44a724571c9ad1385e02f669eca9b2921b22acd0
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-28 13:38:30 -0400

    Hasta la clase de call apply y bind

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
 
 MediaPlayer.prototype.play = function () {
   this.media.play();
@@ -16,6 +25,18 @@ MediaPlayer.prototype.togglePlay = function () {
   } else {
     this.pause();
   }
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+  this.media.muted = !this.media.muted;
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
+const buttonPlay = document.querySelector('#playButton');
+const buttonMute = document.querySelector('#muteButton');
 
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-button.onclick = () => player.togglePlay();
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
